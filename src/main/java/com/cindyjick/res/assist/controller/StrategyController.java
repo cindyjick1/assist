1e57d4776d24bfc27aa81326a084c5c8758d6736
cindyjick1 <1508299639@qq.com>
2023-04-07 00:16:01 +0800

    start|something try

--- src/main/java/com/cindyjick/res/assist/controller/StrategyController.java
+++ src/main/java/com/cindyjick/res/assist/controller/StrategyController.java
@@ -3,12 +3,24 @@ package com.cindyjick.res.assist.controller;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.function.Predicate;
 
 public class StrategyController {
-    public abstract static class Params {
-        private Map<String, Class<?>> param;
-        private Map<String, LinkedList<String>> source;
+    public static class Params {
+        public static final String CONTEXT = "Context";
+        private final Map<String, Class<?>> param;
+        private final Map<String, LinkedList<String>> source;
+
+        public Params(List<String> varCodeList) {
+            this.param = new ConcurrentHashMap<>();
+            this.source = new ConcurrentHashMap<>();
+            Optional.ofNullable(varCodeList).ifPresent(e -> e.forEach(f -> {
+                param.put(f, String.class);
+                source.computeIfAbsent(f, k -> new LinkedList<>()).add(CONTEXT);
+            }));
+        }
     }
 
     public static class StrategyNode {
@@ -31,7 +43,12 @@ public class StrategyController {
         private StrategyNode startNode;
         private Map<String, StrategyNode> endNodes;
         private Map<String, List<OutputDegree>> outputDegree;
+        private Map<String, List<StrategyNode>> inputDegree;
 
+        public Strategy(Params params, Map<String, StrategyNode> nodes) {
+            this.params = params;
+            this.nodes = nodes;
+        }
     }
 
     public static class OutputDegree {
@@ -47,8 +64,14 @@ public class StrategyController {
 
     public StrategyController() {
         // TODO: remove mock data
-        this.strategy = new Strategy();
-        StrategyNode startNode = new StrategyNode("start", "开始", 50, 50);
-
+        var startNode = new StrategyNode("start", "开始", 50, 50);
+        var node1 = new StrategyNode("node1", "节点1", 50, 100);
+        var node2 = new StrategyNode("node1", "节点2", 50, 200);
+        var end = new StrategyNode("end", "结束", 50, 300);
+        this.strategy = new Strategy(new Params(List.of("a", "b", "c")), Map.of(
+                startNode.nodeCode, startNode,
+                node1.nodeCode, node1,
+                node2.nodeCode, node2,
+                end.nodeCode, end));
     }
 }
